42c84cf6b246d2482f95258179343025ed36a47e
Igor Svadkovsky <igor-svadkovsky@yandex.ru>
2022-11-24 10:35:33 +0300

    Initial commit

---
+++ src/main/java/web/config/AppInit.java
@@ -0,0 +1,29 @@
+package web.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // Метод, указывающий на класс конфигурации
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+
+    // Добавление конфигурации, в которой инициализируется ViewResolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+
+    /* Указывает url, на котором будет базироваться приложение */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
